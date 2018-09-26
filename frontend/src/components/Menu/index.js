dcd3002ae4df4b800ec02ea9d5e991397fbf4b1d
GabrielAanesen <31622983+GabrielAanesen@users.noreply.github.com>
2018-09-26 10:52:35 +0200

    added Menu component

---
+++ frontend/src/components/Menu/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Menu = () => (
+  <div>
+    <p>ÖVERSIKT</p>
+    <p>EXTRA MATERIAL</p>
+    <p>DISKUSSION</p>
+    <p>RELATERADE ARTIKLAR</p>
+  </div>
+)
+
+export default Menu;
